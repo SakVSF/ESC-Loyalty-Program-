2295a75d7916d276a2224a93d638c970c58e8e3c
Saakshi Vinod Saraf <sarafsaakshivinod@gmail.com>
2022-08-06 12:12:10 +0800

    add /members for sign in

--- TransferConnect/server/routes/validation.js
+++ TransferConnect/server/routes/validation.js
@@ -1,15 +1,75 @@
-function Validate(regexstr, input) {
-  //returns true if input conforms to regex specified by LP, else false
-  var isValid = true;
-  try {
-    regex = new RegExp(regexstr);
-    return regex.test(input);
-  } catch (e) {
-    isValid = false;
-    console.log("Regex initialization failed, check regex.");
-    return false;
-  }
+const { MongoClient } = require("mongodb");
+
+const mongodb = require("mongodb").MongoClient;
+
+//old function, do not use
+// function Validate(regexstr, input) {
+//     //returns true if input conforms to regex specified by LP, else false
+//     var isValid = true;
+//     try {
+//         regex = new RegExp(regexstr);
+//         return regex.test(input);
+//     } catch (e) {
+//         isValid = false;
+//         console.log("Regex initialization failed, check regex.");
+//         return false;
+//     }
+// }
+
+function validate(loyalty_currency_name, memberid) {
+    let url =
+        "mongodb+srv://hith:chun@merntest0.hqr9i9x.mongodb.net/merntest0?retryWrites=true&w=majority";
+    let query = { "Loyalty Currency Name": loyalty_currency_name };
+    let projection = { "Validation Regex": 1 };
+
+    try {
+        const client = new MongoClient(url);
+        const database = client.db("TransferConnect");
+        const collection = database.collection("MembershipTest");
+
+        const regex = await collection.findOne(query, projection);
+        console.log(regex["Validation Regex"]);
+        client.close();
+
+        var isValid = true;
+        try {
+            regexstr = new RegExp(regex["Validation Regex"]);
+            return regexstr.test(memberid);
+        } catch (e) {
+            isValid = false;
+            console.log("Regex initialization failed, check regex.");
+            return false;
+        }
+    } catch {
+        console.log(err);
+    }
 }
 
-module.exports = {Validate};
-// console.log(Validate("[0-9]{10}", "1567894820"));
+function memberValidate(membershipid) {
+    //returns true if membership id is registered (in database)
+    let url =
+        "mongodb+srv://hith:chun@merntest0.hqr9i9x.mongodb.net/merntest0?retryWrites=true&w=majority";
+    let result = false;
+    mongodb.connect(
+        url,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        (err, client) => {
+            if (err) throw err;
+            client
+                .db("TransferConnect")
+                .collection("Members")
+                .find({})
+                .toArray((err, data) => {
+                    if (err) throw err;
+                    if (membershipid in data) {
+                        result = true;
+                    }
+                    client.close();
+                });
+        }
+    );
+    return result;
+}
+
+module.exports = { validate };
+module.exports = { memberValidate };
